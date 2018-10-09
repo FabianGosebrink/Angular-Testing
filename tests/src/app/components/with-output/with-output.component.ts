8569ad89f3a163f44accb644447e788432816ce7
FabianGosebrink <fabian.gosebrink@outlook.com>
2018-10-09 15:54:14 +0200

    small formatting things

--- tests/src/app/components/with-output/with-output.component.ts
+++ tests/src/app/components/with-output/with-output.component.ts
@@ -3,10 +3,11 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 @Component({
   selector: 'app-with-output',
   templateUrl: './with-output.component.html',
-  styleUrls: ['./with-output.component.css']
+  styleUrls: ['./with-output.component.css'],
 })
 export class WithOutputComponent implements OnInit {
-  @Output() greet = new EventEmitter<string>();
+  @Output()
+  greet = new EventEmitter<string>();
 
   doGreet() {
     this.greet.emit('Hi');
