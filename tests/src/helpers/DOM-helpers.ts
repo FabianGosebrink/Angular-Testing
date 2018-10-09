ccf2250e78536bd343e0edd141a564c048af74df
FabianGosebrink <fabian.gosebrink@outlook.com>
2018-10-09 15:42:45 +0200

    formatting dom helpers

--- tests/src/helpers/DOM-helpers.ts
+++ tests/src/helpers/DOM-helpers.ts
@@ -1,16 +1,10 @@
 import { ComponentFixture } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 
-export function queryDebugElement<T>(
-  fixture: ComponentFixture<T>,
-  searchItem: string
-) {
+export function queryDebugElement<T>(fixture: ComponentFixture<T>, searchItem: string) {
   return fixture.debugElement.query(By.css(searchItem));
 }
 
-export function getInnerHtml<T>(
-  fixture: ComponentFixture<T>,
-  searchItem: string
-) {
+export function getInnerHtml<T>(fixture: ComponentFixture<T>, searchItem: string) {
   return fixture.debugElement.query(By.css(searchItem)).nativeElement.innerHTML;
 }
