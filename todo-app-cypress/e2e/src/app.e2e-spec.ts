92254006269699a4cb9f1e8620df04b12fafe45d
FabianGosebrink <fabian.gosebrink@outlook.com>
2019-02-26 23:57:10 +0100

    updated to new cli

--- todo-app-cypress/e2e/src/app.e2e-spec.ts
+++ todo-app-cypress/e2e/src/app.e2e-spec.ts
@@ -1,6 +1,8 @@
+import * as fs from 'fs';
+import { browser } from 'protractor/built';
 import { AppPage } from './app.po';
 
-describe('workspace-project App', () => {
+describe('todo-app App', () => {
   let page: AppPage;
 
   beforeEach(() => {
@@ -9,6 +11,52 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to todo-app-cypress!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+
+  it('should add Todo in List', () => {
+    page.navigateTo();
+    expect(page.getListCount()).toBe(0);
+    page.getTextField().clear();
+    page.getTextField().sendKeys('exampleTodo');
+
+    browser.takeScreenshot().then(png => {
+      const stream = fs.createWriteStream(`screenshot-1.png`);
+      stream.write(new Buffer(png, 'base64'));
+      stream.end();
+    });
+
+    page.getButton().click();
+
+    browser.takeScreenshot().then(png => {
+      const stream = fs.createWriteStream('screenshot-2.png');
+      stream.write(new Buffer(png, 'base64'));
+      stream.end();
+    });
+
+    expect(page.getListCount()).toBe(1);
+  });
+
+  it('should add correct css class when marked as done', () => {
+    page.navigateTo();
+    page.getTextField().clear();
+    page.getTextField().sendKeys('exampleTodo');
+
+    browser.takeScreenshot().then(png => {
+      const stream = fs.createWriteStream('screenshot-3.png');
+      stream.write(new Buffer(png, 'base64'));
+      stream.end();
+    });
+
+    page.getButton().click();
+    page.getTodoItemButton().click();
+
+    browser.takeScreenshot().then(png => {
+      const stream = fs.createWriteStream('screenshot-4.png');
+      stream.write(new Buffer(png, 'base64'));
+      stream.end();
+    });
+
+    expect(page.getInactiveElements().count()).toBe(1);
   });
 });
--- todo-app-cypress/e2e/src/app.po.ts
+++ todo-app-cypress/e2e/src/app.po.ts
@@ -8,4 +8,28 @@ export class AppPage {
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
   }
+
+  getTextField() {
+    return element(by.css('input'));
+  }
+
+  getButton() {
+    return element(by.css('button'));
+  }
+
+  getTodoItemButton() {
+    return element(by.id('doneButton'));
+  }
+
+  getListCount() {
+    return element.all(by.css('ul li')).count();
+  }
+
+  getList() {
+    return element.all(by.css('ul li'));
+  }
+
+  getInactiveElements() {
+    return element.all(by.css('.inactive'));
+  }
 }
