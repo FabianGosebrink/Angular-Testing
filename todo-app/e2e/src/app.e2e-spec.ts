0c9d145a6a1cdf1ccc96e4bc41d1c805121001f4
Fabian Gosebrink <fabian.gosebrink@outlook.com>
2020-10-13 09:29:41 +0200

    adding protractor e2e tests

--- todo-app/e2e/src/app.e2e-spec.ts
+++ todo-app/e2e/src/app.e2e-spec.ts
@@ -1,5 +1,6 @@
-import { AppPage } from './app.po';
+import * as fs from 'fs';
 import { browser, logging } from 'protractor';
+import { AppPage } from './app.po';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -10,14 +11,62 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('todo-app app is running!');
+    expect(page.getParagraphText()).toEqual('Welcome to todo-app!');
+  });
+
+  it('should add Todo in List', () => {
+    page.navigateTo();
+    expect(page.getListCount()).toBe(0);
+    page.getTextField().clear();
+    page.getTextField().sendKeys('exampleTodo');
+
+    browser.takeScreenshot().then((png) => {
+      const stream = fs.createWriteStream(`screenshot-1.png`);
+      stream.write(new Buffer(png, 'base64'));
+      stream.end();
+    });
+
+    page.getButton().click();
+
+    browser.takeScreenshot().then((png) => {
+      const stream = fs.createWriteStream('screenshot-2.png');
+      stream.write(new Buffer(png, 'base64'));
+      stream.end();
+    });
+
+    expect(page.getListCount()).toBe(1);
+  });
+
+  it('should add correct css class when marked as done', () => {
+    page.navigateTo();
+    page.getTextField().clear();
+    page.getTextField().sendKeys('exampleTodo');
+
+    browser.takeScreenshot().then((png) => {
+      const stream = fs.createWriteStream('screenshot-3.png');
+      stream.write(new Buffer(png, 'base64'));
+      stream.end();
+    });
+
+    page.getButton().click();
+    page.getTodoItemButton().click();
+
+    browser.takeScreenshot().then((png) => {
+      const stream = fs.createWriteStream('screenshot-4.png');
+      stream.write(new Buffer(png, 'base64'));
+      stream.end();
+    });
+
+    expect(page.getInactiveElements().count()).toBe(1);
   });
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
+    expect(logs).not.toContain(
+      jasmine.objectContaining({
+        level: logging.Level.SEVERE,
+      } as logging.Entry)
+    );
   });
 });
--- todo-app/e2e/src/app.po.ts
+++ todo-app/e2e/src/app.po.ts
@@ -1,11 +1,35 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo(): Promise<unknown> {
-    return browser.get(browser.baseUrl) as Promise<unknown>;
+  navigateTo() {
+    return browser.get('/');
   }
 
-  getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+  getParagraphText() {
+    return element(by.css('app-root h1')).getText();
+  }
+
+  getTextField() {
+    return element(by.css('input'));
+  }
+
+  getButton() {
+    return element(by.css('button'));
+  }
+
+  getTodoItemButton() {
+    return element(by.id('doneButton'));
+  }
+
+  getListCount() {
+    return element.all(by.css('ul li')).count();
+  }
+
+  getList() {
+    return element.all(by.css('ul li'));
+  }
+
+  getInactiveElements() {
+    return element.all(by.css('.inactive'));
   }
 }
