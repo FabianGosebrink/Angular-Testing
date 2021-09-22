d161406f5ae0c191ce8f058bbe1ca3055c2eaf84
FabianGosebrink <fabian.gosebrink@outlook.com>
2021-09-22 10:18:22 +0200

    Improved some tests

--- tests/src/app/components/with-external-service/with-external-service.component.spec.ts
+++ tests/src/app/components/with-external-service/with-external-service.component.spec.ts
@@ -2,7 +2,7 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { BrowserModule } from '@angular/platform-browser';
 import { of } from 'rxjs';
-import { delay } from 'rxjs/operators';
+
 import { getDebugElement, getInnerHtml } from '../../../helpers/DOM-helpers';
 import { CustomHttpService } from '../../services/http-service/http.service';
 import { WithExternalServiceComponent } from './with-external-service.component';
@@ -35,32 +35,27 @@ describe('WithExternalServiceComponent', () => {
   });
 
   it(
-    '(IntegrationTest) should get data when loaded',
+    'should get data when loaded',
     waitForAsync(() => {
       expect(
         getDebugElement<WithExternalServiceComponent>(fixture, 'span')
       ).toBeFalsy();
 
-      const spy = spyOn(service, 'getSingle').and.returnValue(
-        of(responseObject).pipe(delay(500))
+      spyOn(service, 'getSingle').and.returnValue(
+        of(responseObject)
       );
       fixture.detectChanges(); // ngOnInit()
 
-      fixture.whenStable().then(() => {
-        fixture.detectChanges();
-        expect(spy.calls.any()).toBe(true, 'getSingle called');
-        expect(
-          getDebugElement<WithExternalServiceComponent>(fixture, 'span')
-        ).toBeDefined();
-        expect(
-          getDebugElement<WithExternalServiceComponent>(fixture, 'pre')
-        ).toBeDefined();
-        expect(component.result$).toBeDefined();
+      expect(
+        getDebugElement<WithExternalServiceComponent>(fixture, 'span')
+      ).toBeDefined();
+      expect(
+        getDebugElement<WithExternalServiceComponent>(fixture, 'pre')
+      ).toBeDefined();
 
-        expect(getInnerHtml<WithExternalServiceComponent>(fixture, 'pre')).toBe(
-          'Luke Skywalker'
-        );
-      });
+      expect(getInnerHtml<WithExternalServiceComponent>(fixture, 'pre')).toBe(
+        'Luke Skywalker'
+      );
     })
   );
 });
