de5c54a6bc059a71fe187be62f31587e088fd8ce
FabianGosebrink <fabian.gosebrink@outlook.com>
2023-10-18 22:06:33 +0200

    added tests for customer Service

--- tests-jest/src/app/services/http-base-service/customer.service.spec.ts
+++ tests-jest/src/app/services/http-base-service/customer.service.spec.ts
@@ -1,5 +1,6 @@
-import { TestBed } from '@angular/core/testing';
-import { provideMock } from 'src/app/helpers/auto-mock';
+import { TestBed, waitForAsync } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { provideMock } from '../../helpers/auto-mock';
 import { CustomerService } from './customer.service';
 import { HttpService } from './http.service';
 
@@ -15,4 +16,18 @@ describe('CustomerService', () => {
     customerService = TestBed.inject(CustomerService);
     httpService = TestBed.inject(HttpService);
   });
+
+  it('should get all customers', waitForAsync(() => {
+    const getAllCustomersSpy = jest
+      .spyOn(httpService, 'get')
+      .mockReturnValue(of([]));
+
+    const result$ = customerService.getAllCustomers();
+
+    result$.subscribe((result) => {
+      expect(getAllCustomersSpy).toHaveBeenCalledTimes(1);
+      expect(result).toEqual([]);
+      expect(getAllCustomersSpy).toHaveBeenCalledWith('get-all-customers');
+    });
+  }));
 });
