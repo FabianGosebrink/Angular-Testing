fe3082c2c9788e207ab23453adef75f1757cc108
FabianGosebrink <fabian.gosebrink@outlook.com>
2024-03-11 19:19:39 +0100

    Delete unused files and configurations

--- tests-jest/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { importProvidersFrom } from '@angular/core';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-
-bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(BrowserModule)],
-}).catch((err) => console.error(err));
