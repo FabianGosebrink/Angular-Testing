097ccd96f3a95d5ad04bbeefbcdee837bcac5022
FabianGosebrink <fabian.gosebrink@outlook.com>
2024-03-19 11:38:00 +0100

    Reorder imports in http.service.ts

--- tests/src/app/services/http-base-service/http.service.ts
+++ tests/src/app/services/http-base-service/http.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
